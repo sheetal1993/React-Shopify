e4a2ff623442ae776d932af70f9cfc207591a33a
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-03-08 14:57:15 +0530

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,6 @@ const Footer = () => {
       <Grid templateColumns={["repeat(1, 1fr)","repeat(3, 1fr)"]} color="white" fontWeight='bold'>
         <Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-5_-_trans_1.png?v=1610123549" />
         <VStack p="2rem">
-          <Link to="/">The Green Blast</Link>
           <Link to="/">The Blue Berry</Link>
           <Link to="/">The Yellow Mellow</Link>
         </VStack>
